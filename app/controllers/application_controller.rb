f7aceff7c0347090580c8c4633815662acc364e9
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-07-19 16:51:38 +0000

    [update]view.show,index,about

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sigin_in_path_for(resource)
-    users_path(params[:id])
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as:"about"
 
-  resources :books, only: [:index, :show, :edit, :create, :destroy]
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
