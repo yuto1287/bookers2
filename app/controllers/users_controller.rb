5039ec405a6f00e6be6362925621eab4c1ed3d33
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-07-23 15:41:56 +0000

    [add]flasfmessage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book =Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -15,6 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
     redirect_to user_path(params[:id])
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
 
-  has_one_attached :image
   belongs_to :user
+
+  validates :title, presence: true
+  validates :opinion, presence: true
+
 end
